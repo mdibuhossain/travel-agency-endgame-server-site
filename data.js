1eedc46d39043324a03aa616120ad0cdb5e09d25
Ibrahim H <mohammad.ihakash@gmail.com>
2021-11-01 05:34:28 +0600

    services added

---
+++ data.js
@@ -0,0 +1,38 @@
+[
+    {
+        "title": "Santorini Have Conquered a Corner of Your Imagination Before You’ve Set Eyes On It.",
+        "date": "April 5, 2016",
+        "comments": 2,
+        "img": "https://gotravel.qodeinteractive.com/wp-content/uploads/2016/04/blog-image-1-600x480.jpg"
+    },
+    {
+        "title": "These Are The Best Travel Tips I’ve Discovered Along The Way On Journey",
+        "date": "October 5, 2016",
+        "comments": 5,
+        "img": "https://gotravel.qodeinteractive.com/wp-content/uploads/2016/04/blog-image-2-600x480.jpg"
+    },
+    {
+        "title": "With Samba and Bossa Nova it’s easy to fall in love with Rio de Janeiro.",
+        "date": "April 5, 2016",
+        "comments": 9,
+        "img": "https://gotravel.qodeinteractive.com/wp-content/uploads/2016/04/blog-image-3-2-600x480.jpg"
+    },
+    {
+        "title": "A Gondola Ride is the One Venetian Tourist Trap Everyone Falls Into",
+        "date": "July 5, 2016",
+        "comments": 5,
+        "img": "https://gotravel.qodeinteractive.com/wp-content/uploads/2016/04/blog-image-4-600x480.jpg"
+    },
+    {
+        "title": "Ten Practical Ways to Save Money in Argentina If You Are a Tourist From Europe",
+        "date": "March 5, 2016",
+        "comments": 7,
+        "img": "https://gotravel.qodeinteractive.com/wp-content/uploads/2016/04/blog-image-5-600x480.jpg"
+    },
+    {
+        "title": "Fountain Di Trevi Is Made From The Same Material As The Colosseum",
+        "date": "April 5, 2016",
+        "comments": 1,
+        "img": "https://gotravel.qodeinteractive.com/wp-content/uploads/2016/04/blog-image-5-600x480.jpg"
+    }
+]
---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// midware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5p7yt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('WorldTrip');
+        const serviceCollection = database.collection('service');
+        const blogPostCollection = database.collection('blogPost');
+
+        app.get('/blog', async (req, res) => {
+            const cursor = blogPostCollection.find({});
+            const blog = await cursor.toArray();
+            res.send(blog);
+        })
+
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // POST API
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            console.log('hit the post api', service);
+            const result = await serviceCollection.insertOne(service);
+            console.log(result);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log('listening PORT', port);
+})
