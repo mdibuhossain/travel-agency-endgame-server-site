fd39c2e43bfceed7c2924575c8e7b736ac6961db
Ibrahim H <mohammad.ihakash@gmail.com>
2021-11-02 16:24:45 +0600

    error fixed

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ async function run() {
         app.delete('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const res  = await serviceCollection.deleteOne(query);
-            res.json(res);
+            const finalRes  = await serviceCollection.deleteOne(query);
+            res.json(finalRes);
         })
 
         // Get Blogs
